fed747f8b010935738394c28ebd46b07a459ba20
ErikPrograms <erikdstafford@gmail.com>
2022-02-08 20:38:38 -0500

    User Table Created

---
+++ Cardz/Stored Procedures/dbo.AddNewUser.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[AddNewUser]
+	@pUsername NVARCHAR(50),
+	@pPassword NVARCHAR(50),
+	@pDisplayName NVARCHAR(50),
+	@response NVARCHAR(250) OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	DECLARE @salt UNIQUEIDENTIFIER=NEWID()
+	BEGIN TRY
+
+	INSERT INTO dbo.[User] (Username, PasswordHash, Salt, DisplayName)
+	VALUES(@pUsername, HASHBYTES('SHA2_512', @pPassword+CAST(@salt AS NVARCHAR(36))), @salt, @pDisplayName)
+	
+	SET @response='Success'
+
+	END TRY
+	BEGIN CATCH
+		SET @response=ERROR_MESSAGE()
+	END CATCH
+END
